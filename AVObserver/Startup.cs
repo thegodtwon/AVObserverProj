99d93ff84a7edc6e2e619acebada3ac2637b1e1f
Kris Hahn <kris.hahn@javs.com>
2017-07-14 14:23:18 -0400

    Initial Code Commit

---
+++ AVObserver/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AVObserver.Startup))]
+namespace AVObserver
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
